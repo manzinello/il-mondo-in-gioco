ef1c9312c7c183a6fb2e52db330c5c75c28596f1
Matteo Manzinello <matteo@manzinello.com>
2018-11-10 11:33:57 +0100

    Bozza di components, caricate immagini

--- src/App.js
+++ src/App.js
@@ -1,63 +1,40 @@
 import React, { Component } from "react";
-import { Attraction, Car } from "grommet-icons";
 
-import { Grommet, Anchor, Box, Button, Text } from "grommet";
+import { Grommet, Box, Text } from "grommet";
+import MondoBox from "./components/MondoBox";
 
-const SimpleBox = () => (
-  <Grommet full>
-    <Box
-      direction="row-responsive"
-      justify="center"
-      align="center"
-      pad="xlarge"
-      background="dark-2"
-      gap="medium"
-    >
-      <Box
-        pad="large"
-        align="center"
-        background={{ color: "light-2", opacity: "strong" }}
-        round
-        gap="small"
-      >
-        <Attraction size="large" />
-        <Text>Party</Text>
-        <Anchor href="" label="Link" />
-        <Button label="Button" onClick={() => {}} />
-      </Box>
-      <Box pad="large" align="center" background="dark-3" round gap="small">
-        <Car size="large" color="light-2" />
-        <Text>Travel</Text>
-        <Anchor href="" label="Link" />
-        <Button label="Button" onClick={() => {}} />
-      </Box>
-      <Box pad="large" align="center" background="dark-3" round gap="small">
-        <Car size="large" color="light-2" />
-        <Text>Travel</Text>
-        <Anchor href="" label="Link" />
-        <Button label="Button" onClick={() => {}} />
-      </Box>
-      <Box pad="large" align="center" background="dark-3" round gap="small">
-        <Car size="large" color="light-2" />
-        <Text>Travel</Text>
-        <Anchor href="" label="Link" />
-        <Button label="Button" onClick={() => {}} />
-      </Box>
-      <Box pad="large" align="center" background="dark-3" round gap="small">
-        <Car size="large" color="light-2" />
-        <Text>Travel</Text>
-        <Anchor href="" label="Link" />
-        <Button label="Button" onClick={() => {}} />
-      </Box>
-      <Box pad="large" align="center" background="dark-3" round gap="small">
-        <Car size="large" color="light-2" />
-        <Text>Travel</Text>
-        <Anchor href="" label="Link" />
-        <Button label="Button" onClick={() => {}} />
-      </Box>
-    </Box>
-  </Grommet>
-);
+const giochi = [
+  {
+    backgroundImage: "gioco-1.jpg",
+    num: 1,
+    gioco: "Mandala"
+  },
+  {
+    backgroundImage: "gioco-2.jpg",
+    num: 2,
+    gioco: "Mandala"
+  },
+  {
+    backgroundImage: "gioco-3.jpg",
+    num: 3,
+    gioco: "Mandala"
+  },
+  {
+    backgroundImage: "gioco-4.jpg",
+    num: 4,
+    gioco: "Mandala"
+  },
+  {
+    backgroundImage: "gioco-5.jpg",
+    num: 5,
+    gioco: "Mandala"
+  },
+  {
+    backgroundImage: "gioco-6.jpg",
+    num: 6,
+    gioco: "Mandala"
+  }
+];
 
 class App extends Component {
   state = {
@@ -68,7 +45,30 @@ class App extends Component {
   render() {
     return (
       <>
-        <SimpleBox />
+        <Grommet full>
+          <Box background="dark-2" fill>
+            <Box>
+              <Text>Ciao</Text>
+            </Box>
+            <Box
+              direction="row-responsive"
+              justify="center"
+              alignSelf="center"
+              align="center"
+              pad="xlarge"
+              gap="medium"
+              fill
+            >
+              {giochi.map(gioco => (
+                <MondoBox
+                  backgroundImage={gioco.backgroundImage}
+                  gioco={gioco.gioco}
+                  num={gioco.num}
+                />
+              ))}
+            </Box>
+          </Box>
+        </Grommet>
       </>
     );
   }
---
+++ src/components/MondoBox.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Heading, Grommet, Anchor, Box, Button, Text } from "grommet";
+
+class MondoBox extends React.Component {
+  render = () => {
+    return (
+      <Box
+        pad="large"
+        align="center"
+        background={{
+          color: "neutral-1",
+          dark: true,
+          opacity: 'weak',
+          position: "bottom",
+          image: "url(images/" + this.props.backgroundImage + ")"
+        }}
+        round
+        gap="small"
+      >
+        <Heading level={1}>{this.props.num}</Heading>
+        <Text>{this.props.gioco}</Text>
+        <Anchor href="" label="Link" />
+        <Button label="Button" onClick={() => {}} />
+      </Box>
+    );
+  };
+}
+
+export default MondoBox;

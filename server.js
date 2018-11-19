78a2862399f0d103ae5aad6cec0011b6496bc7e4
Matteo Manzinello <matteo@manzinello.com>
2018-11-19 14:21:48 +0100

    Bozza di invio tramite socket

--- server.js
+++ server.js
@@ -20,19 +20,9 @@ var LED = new Gpio(4, "out"); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, "in", "both"); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 */
 
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
-});
-
-app.post("/api/world", (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
-
 http.listen(port, () => {
   console.log(`Listening on port ${port}`);
+  sendButtonPressed(2);
 });
 
 /*

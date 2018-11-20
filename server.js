642d8eb7ac847ae40a7e5dea78aaba0de9d779e6
Matteo Manzinello <matteo@manzinello.com>
2018-11-20 19:02:04 +0100

    Bozza con Gpio doppio

--- server.js
+++ server.js
@@ -6,46 +6,45 @@ const port = process.env.PORT || 5000;
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
+var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 socketInit = () => {
   console.log("button-pressed! " + 3);
   io.on("connection", socket => {
-    socket.emit("button-pressed", { value: 3 });
+    var button1 = new Gpio(4, "in", "falling");
+    var button2 = new Gpio(17, "in", "falling");
+    button1.watch((err, value) => {
+      if (err) {
+        console.error("There was an error", err);
+        return;
+      }
+      console.log("button 1 pressed! - " + value);
+      socket.emit("button-pressed", { value: 1 });
+    });
+    button2.watch((err, value) => {
+      if (err) {
+        console.error("There was an error", err);
+        return;
+      }
+      console.log("button 2 pressed! - " + value);
+      socket.emit("button-pressed", { value: 2 });
+    });
   });
 };
 
-/*
-var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(4, "out"); //use GPIO pin 4 as output
-var pushButton = new Gpio(17, "in", "both"); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
-*/
+function unexportOnClose() {
+  //function to run when exiting program
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport LED GPIO to free resources
+  pushButton.unexport(); // Unexport Button GPIO to free resources
+}
+
+process.on("SIGINT", unexportOnClose); //function to run when user closes using ctrl+c
 
 http.listen(port, () => {
   console.log(`Listening on port ${port}`);
   socketInit();
 });
-
-/*
-  start = () => {
-    pushButton.watch(function(err, value) {
-      //Watch for hardware interrupts on pushButton GPIO, specify callback function
-      if (err) {
-        //if an error
-        console.error("There was an error", err); //output error message to console
-        return;
-      }
-      LED.writeSync(value); //turn LED on or off depending on the button state (0 or 1)
-    });
-
-    function unexportOnClose() {
-      //function to run when exiting program
-      LED.writeSync(0); // Turn LED off
-      LED.unexport(); // Unexport LED GPIO to free resources
-      pushButton.unexport(); // Unexport Button GPIO to free resources
-    }
-
-    process.on("SIGINT", unexportOnClose); //function to run when user closes using ctrl+c
-  };
-  */
